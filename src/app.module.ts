d16ec5b7c57d631154860db1162bb68438781585
Yuni-Q <lyh6425@gmail.com>
2021-03-22 20:27:17 +0900

    앱 버전 체크를 위한 미들웨어 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,17 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { AnswersModule } from './answers/answers.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './common/database/database.module';
 import { EnvModule } from './common/env/env.module';
 import { LoggerMiddleware } from './common/middlewares/logger.middleware';
-import { UsersModule } from './users/users.module';
-import { SigninController } from './signin/signin.controller';
-import { SigninModule } from './signin/signin.module';
-import { QuestionsModule } from './questions/questions.module';
-import { MissionsModule } from './missions/missions.module';
-import { AnswersController } from './answers/answers.controller';
-import { AnswersModule } from './answers/answers.module';
+import { VersionMiddleware } from './common/middlewares/version.middleware';
 import { FilesModule } from './files/files.module';
+import { MissionsModule } from './missions/missions.module';
+import { QuestionsModule } from './questions/questions.module';
+import { SigninModule } from './signin/signin.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -32,5 +31,6 @@ import { FilesModule } from './files/files.module';
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer): any {
     consumer.apply(LoggerMiddleware).forRoutes('*');
+    consumer.apply(VersionMiddleware).forRoutes('api/v1');
   }
 }
