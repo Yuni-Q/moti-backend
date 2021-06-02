25b9562f032804332f10a5a6b83fdf038e2b42ec
Yuni-Q <lyh6425@gmail.com>
2021-06-02 09:14:04 +0900

    루트 route 제거

--- src/backend/app.controller.ts
+++ src/backend/app.controller.ts
@@ -13,24 +13,24 @@ import { UndefinedToNullInterceptor } from './common/interceptors/undefined.inte
 @ApiTags('참고')
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
-  @Get()
-  getHello(): any {
-    try {
-      throw new InvalidQueryException();
-    } catch (e) {
-      return 'OK';
-      throw new HttpException('No', 400);
-    }
-  }
+  // @Get()
+  // getHello(): any {
+  //   try {
+  //     throw new InvalidQueryException();
+  //   } catch (e) {
+  //     return 'OK';
+  //     throw new HttpException('No', 400);
+  //   }
+  // }
   @Get('health')
   getHealth(): any {
     return 'OK';
   }
 
-  @Get('error')
-  error(): any {
-    throw new Error('error');
-  }
+  // @Get('error')
+  // error(): any {
+  //   throw new Error('error');
+  // }
 }
