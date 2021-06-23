b7960a70bd51ecc059731821d4efc9d192cf045b
Yuni-Q <lyh6425@gmail.com>
2021-06-23 23:24:12 +0900

    ValidateService 전에 시간 증가가

--- scripts/validate_server.sh
+++ scripts/validate_server.sh
@@ -2,7 +2,7 @@ echo '======================='
 echo 'Running validate_server'
 echo '======================='
 
-sleep 3
+sleep 10
 result=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:8000/health)
 
 echo Check http://localhost:8000/health
