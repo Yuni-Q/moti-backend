4896a8531d3ced35f48404353f3d05a0fa86dcc0
Yuni-Q <lyh6425@gmail.com>
2021-06-27 15:22:12 +0900

    front test code를 위한 준비

--- src/backend/app.controller.ts
+++ src/backend/app.controller.ts
@@ -1,9 +1,4 @@
-import {
-  Controller,
-  Get,
-  Param,
-  UseInterceptors
-} from '@nestjs/common';
+import { Controller, Get, UseInterceptors } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
 import { UndefinedToNullInterceptor } from './common/interceptors/undefined.interceptor';
@@ -12,7 +7,7 @@ import { UndefinedToNullInterceptor } from './common/interceptors/undefined.inte
 @ApiTags('참고')
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) { }
+  constructor(private readonly appService: AppService) {}
 
   // @Get()
   // getHello(): any {
@@ -24,14 +19,8 @@ export class AppController {
   //   }
   // }.
 
-  
-   /**
-   * 테스트
-   * @param page 페이지(1~). 기본값: 1
-   * @param limit 페이지 당 엔티티 수. 기본값 10
-   */
   @Get('health')
-  getHealth(@Param('page') page, @Param('limit') limit): any {
+  getHealth(): any {
     return 'OK';
   }
 
--- src/front/components/Error.test.tsx
+++ src/front/components/Error.test.tsx
@@ -3,41 +3,34 @@ import { useRouter } from 'next/router';
 import React from 'react';
 import Error from './Error';
 
-jest.mock('next/router')
+jest.mock('next/router');
 
-describe("Error", () => {
-	context('with errorMessage', () => {
-		it('render error', () => {
-			const { container } = render((
-				<Error errorMessage='error' />
-				))
-				expect(container).toHaveTextContent("재접속")
-				expect(container).toHaveTextContent("알 수 없는 오류가 발생했습니다.")
-		})
-		it('click 재접속 button', () => {
-			const reload = jest.fn();
-			(useRouter as jest.Mock).mockImplementation(() => {
-				return {
-					reload
-				}
-			});
-			const { getAllByText } = render((
-				<Error errorMessage='error' />
-				))
-				const buttons = getAllByText('재접속');
-				fireEvent.click(buttons[0]);
-				expect(reload).toBeCalled();
-		})
-	})
-	context('without errorMessage', () => {
-		it('render error', () => {
-			const { container } = render((
-				<Error />
-				))
-				expect(container).toHaveTextContent("재접속")
-				expect(container).toHaveTextContent("인터넷이 불안정해요.")
-				expect(container).toHaveTextContent("확인 후 재접속 해주세요.")
-		})
-	})
-	
-})
+describe('Error', () => {
+  context('with errorMessage', () => {
+    it('render error', () => {
+      const { container } = render(<Error errorMessage="error" />);
+      expect(container).toHaveTextContent('재접속');
+      expect(container).toHaveTextContent('알 수 없는 오류가 발생했습니다.');
+    });
+    it('click 재접속 button', () => {
+      const reload = jest.fn();
+      (useRouter as jest.Mock).mockImplementation(() => {
+        return {
+          reload,
+        };
+      });
+      const { getAllByText } = render(<Error errorMessage="error" />);
+      const buttons = getAllByText('재접속');
+      fireEvent.click(buttons[0]);
+      expect(reload).toBeCalled();
+    });
+  });
+  context('without errorMessage', () => {
+    it('render error', () => {
+      const { container } = render(<Error />);
+      expect(container).toHaveTextContent('재접속');
+      expect(container).toHaveTextContent('인터넷이 불안정해요.');
+      expect(container).toHaveTextContent('확인 후 재접속 해주세요.');
+    });
+  });
+});
