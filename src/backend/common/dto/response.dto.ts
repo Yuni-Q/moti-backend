6f33fca963a059ff408382c20ef084b78ed4e72c
Yuni-Q <lyh6425@gmail.com>
2021-06-29 10:44:14 +0900

    build 시 상대경로 제거를 위해 tpye을 좀더 넓게 잡기

--- src/backend/common/dto/response.dto.ts
+++ src/backend/common/dto/response.dto.ts
@@ -1,16 +1,18 @@
 import { HttpStatus } from '@nestjs/common';
 import { ApiProperty } from '@nestjs/swagger';
 
+const status = HttpStatus.OK;
+
 export class ResponseDto {
   @ApiProperty({
-    example: HttpStatus.OK,
+    example: status,
     description: 'http 상태 코드',
     required: true,
   })
   public status?: number;
 
   @ApiProperty({
-    example: HttpStatus.OK,
+    example: status,
     description: 'custom 상태 코드',
     required: true,
   })
